83f8a96872ce32b3e2689107fd36230d4773d24e
rjrom <108962918+rjrom@users.noreply.github.com>
2023-03-21 18:45:30 +0800

    collapse navbar

--- js/main.js
+++ js/main.js
@@ -77,3 +77,11 @@
     
 })(jQuery);
 
+
+// Collapse navbar on clicking link
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarCollapse')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
